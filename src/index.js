0605cf401ffb6a161a4c68166f14a6c748f40621
여율 <dbfm2222@gmail.com>
2020-12-02 22:59:00 +0900

    백업1 아차할때날라간다조심하자

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+const chooseNumber = document.querySelector(".choose-number");
+const betweenNumber = document.querySelector(".between-number");
+const sliderVal = document.querySelector(".random-slider");
+const vsResult = document.querySelector(".js-result");
+
+function selectRange() {
+  const max = sliderVal.value; // 최대값 받아오기
+  chooseNumber.setAttribute("max", max);
+  betweenNumber.innerHTML = "Generate a number between";
+  betweenNumber.innerHTML += " 0 and " + max;
+}
+
+function clickPlay(event) {
+  const playBtn = event.target;
+  const myChosen = chooseNumber.innerText;
+  if (myChosen === Math.random()) {
+    vsResult.innerHTML = "You won!";
+  } else {
+  }
+}
+
+function init() {}
+
+init();
+document.addEventListener("mousemove", selectRange);

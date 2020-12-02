a4e8e9c8727889f517c06e6b21c719730fc05e1c
여율 <dbfm2222@gmail.com>
2020-12-02 23:26:44 +0900

    완성 구현완성~~!

--- src/index.js
+++ src/index.js
@@ -5,24 +5,47 @@ const chooseNumber = document.querySelector(".choose-number");
 const betweenNumber = document.querySelector(".between-number");
 const sliderVal = document.querySelector(".random-slider");
 const vsResult = document.querySelector(".js-result");
+const playBtn = document.querySelector(".play-button");
+
+function filterRange() {
+  // 최대값 넘지 못하게 필터링 해주는 함수
+  const max = sliderVal.value;
+  if (max < chooseNumber.value) {
+    chooseNumber.value = max;
+  }
+}
 
 function selectRange() {
   const max = sliderVal.value; // 최대값 받아오기
   chooseNumber.setAttribute("max", max);
-  betweenNumber.innerHTML = "Generate a number between";
-  betweenNumber.innerHTML += " 0 and " + max;
+  betweenNumber.innerHTML = `Generate a number between 0 and ${max}`;
 }
 
-function clickPlay(event) {
-  const playBtn = event.target;
-  const myChosen = chooseNumber.innerText;
-  if (myChosen === Math.random()) {
-    vsResult.innerHTML = "You won!";
+function clickPlay() {
+  const max = sliderVal.value; // 최대값 받아오기
+  const myChosen = chooseNumber.value;
+  const machineChosen = Math.floor(Math.random() * max); // 난수 발생
+  if (myChosen === machineChosen) {
+    vsResult.innerHTML =
+      "You chose: " +
+      myChosen +
+      ", the machine chose: " +
+      machineChosen +
+      `<br>` +
+      `<b>` +
+      "You won!";
   } else {
+    vsResult.innerHTML =
+      "You chose: " +
+      myChosen +
+      ", the machine chose: " +
+      machineChosen +
+      `<br>` +
+      `<b>` +
+      "You lose.";
   }
 }
 
-function init() {}
-
-init();
 document.addEventListener("mousemove", selectRange);
+playBtn.addEventListener("click", clickPlay);
+chooseNumber.addEventListener("keyup", filterRange);
